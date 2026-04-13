ccbfeb5ac8f263a452747cb8f7ae67a243e6f3eb
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2026-04-13 12:53:24 +0530

    create album songs section and song bar components

---
+++ src/components/Song/SongBar.jsx
@@ -0,0 +1,74 @@
+import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+
+function SongBar({ song }) {
+  const [duration, setDuration] = useState({
+    minutes: 0,
+    seconds: 0,
+  });
+
+  const calculateSongDuration = () => {
+    const totalDuration = song.durationInMs;
+
+    const minutes = Math.floor(totalDuration / (1000 * 60));
+    const seconds = Math.floor((totalDuration % (1000 * 60)) / 1000);
+
+    return { minutes: minutes, seconds: seconds };
+  };
+
+  useEffect(() => {
+    const onLoadHandler = () => {
+      const totalDuration = calculateSongDuration();
+
+      setDuration((prev) => ({
+        ...prev,
+        minutes: totalDuration.minutes,
+        seconds: totalDuration.seconds,
+      }));
+    };
+
+    onLoadHandler();
+  }, []);
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr auto",
+          alignItems: "center",
+          paddingY: 2,
+          borderBottom: 1,
+          cursor: "pointer",
+          "&:hover": {
+            backgroundColor: "rgba(52, 201, 75, 0.1)",
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 2,
+          }}
+        >
+          <img src={song.image} alt="song-profile" width={60} height={60} />
+
+          <Typography>{song.title}</Typography>
+        </Box>
+
+        <Typography sx={{ textAlign: "left" }}>
+          {song.artists.length > 1
+            ? `${song.artists[0]} + ${song.artists.length - 1} more`
+            : `${song.artists[0]}`}
+        </Typography>
+
+        <Typography sx={{ textAlign: "right", paddingLeft: 15 }}>
+          {duration.minutes}:{duration.seconds}
+        </Typography>
+      </Box>
+    </>
+  );
+}
+
+export default SongBar;
--- src/pages/AlbumDetailsPage.jsx
+++ src/pages/AlbumDetailsPage.jsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import BackButtonImage from "../assets/back-button.png";
 import { useNavigate } from "react-router-dom";
 import AlbumDetailsHeader from "../components/Header/AlbumDetailsHeader";
+import AlbumSongsSection from "../components/Song/AlbumSongsSection";
 
 function AlbumDetailsPage() {
   const navigate = useNavigate();
@@ -15,6 +16,8 @@ function AlbumDetailsPage() {
       <img src={BackButtonImage} alt="back-button" onClick={handleClick} />
 
       <AlbumDetailsHeader />
+
+      <AlbumSongsSection />
     </Box>
   );
 }
